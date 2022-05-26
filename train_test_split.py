aa4f2af437cd83015d9c9090753d7d90ab471f6b
Shahbaz Khader <shahbazkhader@gmail.com>
2022-05-26 21:23:14 +0200

    feat: splits data into validation set also

--- train_test_split.py
+++ train_test_split.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pandas import value_counts
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -15,8 +16,8 @@ class TrainTestSplitFBMarketData():
 
     def train_test_split(self, df, ratio):
         """ Splits and returns the two datasets """
-        split = StratifiedShuffleSplit(
-            n_splits=1, test_size=ratio, random_state=42)
+        split_1 = StratifiedShuffleSplit(
+            n_splits=1, test_size=ratio[2], random_state=42)
 
         # use this piple line to extract the product catagory level that 
         # is used for the stratified shuffle
@@ -37,12 +38,29 @@ class TrainTestSplitFBMarketData():
         # assign 'NA' to all ctaegories with counts less than the value
         # required to produce at least one test sample
         cat_counts = cat_col.value_counts()
-        insig_cats = cat_counts[cat_counts < int(1/ratio)]
+        insig_cats = cat_counts[cat_counts < int(1/ratio[2])]
         insig_cat_dict = {cat:'NA' for cat in insig_cats.index}
         cat_col = cat_col.replace(insig_cat_dict)
 
-        # split the data
-        for train_index, test_index in split.split(df, cat_col):
-            strat_train_set = df.loc[train_index]
-            strat_test_set = df.loc[test_index]
-        return strat_train_set, strat_test_set
+        # split the data into train and test
+        for train_index, test_index in split_1.split(df.to_numpy(), cat_col.to_numpy()):
+            strat_train_set = df.iloc[train_index]
+            strat_test_set = df.iloc[test_index]
+            strat_cat_col = cat_col.iloc[train_index]
+        
+        split_2 = StratifiedShuffleSplit(
+            n_splits=1, test_size=ratio[1]/(ratio[0] + ratio[1]), random_state=42)
+
+        # split the train data into train and val
+        for train_index, val_index in split_2.split(strat_train_set.to_numpy(), strat_cat_col.to_numpy()):
+            strat_val_set = strat_train_set.iloc[val_index]
+            strat_train_set = strat_train_set.iloc[train_index]
+        
+        # test the validity of the split
+        all_indices = list(strat_train_set.index) + \
+            list(strat_test_set.index) + list(strat_val_set.index)
+        all_indices.sort()
+        test_indeces = list(range(len(all_indices)))
+        assert(all_indices == test_indeces)
+
+        return strat_train_set, strat_val_set, strat_test_set
